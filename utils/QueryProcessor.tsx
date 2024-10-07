f29f1e0df91fc48f6a99758e6616db8bf60a0ce2
Veronica Pimenova <vcpime03@gmail.com>
2024-10-07 10:32:17 -0400

    added question for what is andrewid

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,5 +1,7 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("shakespeare")) {
+  const lowerQuery = query.toLowerCase();
+
+  if (lowerQuery.includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
@@ -7,5 +9,9 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
-  return "";
+  if (lowerQuery.includes("what is your andrew id")) {
+    return "vpimenov"; 
+  }
+
+  return "I'm sorry, I don't understand the question.";
 }
